f40d94aaafadb4c4ba804312c768165a8eacb0dc
Alexander <wryyy906@gmail.com>
2016-07-01 11:32:27 +0300

    Added foundation for tests

---
+++ beretained-processor/src/test/java/com/woodblockwithoutco/beretained/PrimitiveRetainedFieldsTest.java
@@ -0,0 +1,14 @@
+package com.woodblockwithoutco.beretained;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+
+public class PrimitiveRetainedFieldsTest {
+
+    @Test
+    public void testPrimitiveCodeGeneration() {
+        Assert.assertTrue(true);
+    }
+}
